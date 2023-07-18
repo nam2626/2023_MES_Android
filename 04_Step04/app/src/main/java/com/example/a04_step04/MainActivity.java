e2162d0ecb910fd5ff281bdab621543f1beded9c
nam2626 <nam2626@gmail.com>
2023-07-18 16:43:54 +0900

    뷰, 이벤트 처리

--- 04_Step04/app/src/main/java/com/example/a04_step04/MainActivity.java
+++ 04_Step04/app/src/main/java/com/example/a04_step04/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.a04_step04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnOK;
+    Button btnExit;
+    EditText txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnOK = findViewById(R.id.btn_ok);
+        btnExit = findViewById(R.id.btn_exit);
+        txt = findViewById(R.id.txt);
+
+        //버튼 클릭시 입력한 내용을 Toast 메세지로 출력
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, txt.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAndRemoveTask(); // 종료시 태스크 리스트에 남지 않음
+                //finish(); //종료가 되도 태스크 리스트에 남음
+            }
+        });
+
     }
 }
+
+
+
+
+
+
+
+
