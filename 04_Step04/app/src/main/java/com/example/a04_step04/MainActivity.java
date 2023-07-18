f9d1dc8d89137de50cb6608b9afb408afb8963e5
nam2626 <nam2626@gmail.com>
2023-07-18 16:52:34 +0900

    뷰, 이벤트 처리

--- 04_Step04/app/src/main/java/com/example/a04_step04/MainActivity.java
+++ 04_Step04/app/src/main/java/com/example/a04_step04/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.a04_step04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 finishAndRemoveTask(); // 종료시 태스크 리스트에 남지 않음
                 //finish(); //종료가 되도 태스크 리스트에 남음
+            }
+        });
+        //입력된 텍스트가 변했을때 이벤트 처리
+        txt.addTextChangedListener(new TextWatcher() {
+            //charSequence : 입력된 내용
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d("TextEvent", "beforeTextChanged: "+charSequence);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.d("TextEvent", "onTextChanged: "+charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Log.d("TextEvent", "afterTextChanged: "+editable.toString());
             }
         });
 
