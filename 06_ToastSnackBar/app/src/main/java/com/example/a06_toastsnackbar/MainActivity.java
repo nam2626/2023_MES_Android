1ae28183553b634120b56f3949537c1880e7a833
nam2626 <nam2626@gmail.com>
2023-07-19 10:38:36 +0900

    스낵바

--- 06_ToastSnackBar/app/src/main/java/com/example/a06_toastsnackbar/MainActivity.java
+++ 06_ToastSnackBar/app/src/main/java/com/example/a06_toastsnackbar/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a06_toastsnackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText xpos = findViewById(R.id.txt_xpos);
+        EditText ypos = findViewById(R.id.txt_ypos);
+        Button btnToast = findViewById(R.id.btn_toast);
+        Button btnAlert = findViewById(R.id.btn_alert);
+        Button btnSnack = findViewById(R.id.btn_snack);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
