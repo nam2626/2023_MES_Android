47bccb3e0ac01c6f1242bfaff990e40ed32ea913
nam2626 <nam2626@gmail.com>
2023-07-20 11:49:35 +0900

    SeekBar

--- 12_ProgressBar/app/src/main/java/com/example/a12_progressbar/MainActivity.java
+++ 12_ProgressBar/app/src/main/java/com/example/a12_progressbar/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                         t.start();
                     }
                 });
+                dialog.show();
             }
         });
     }
