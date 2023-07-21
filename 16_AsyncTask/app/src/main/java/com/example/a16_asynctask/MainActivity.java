5842ed80ba3f4f5d92f8eb5e3b5f3413c58bce1d
nam2626 <nam2626@gmail.com>
2023-07-21 14:43:57 +0900

    AsyncTask

--- 16_AsyncTask/app/src/main/java/com/example/a16_asynctask/MainActivity.java
+++ 16_AsyncTask/app/src/main/java/com/example/a16_asynctask/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a16_asynctask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
@@ -20,7 +21,13 @@ public class MainActivity extends AppCompatActivity {
         btnStart = findViewById(R.id.btn_start);
         btnCancel = findViewById(R.id.btn_cancel);
 
-
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressThread thread = new ProgressThread();
+                thread.start();
+            }
+        });
     }
 
     public class ProgressThread extends Thread{
@@ -29,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
             //프로그레스 바를 100까지 자동으로 채우는 코드
             //한번 증가할 떄 5씩 증가
             //증가후 1초동안 쉬었다가 다시 진행 - Thread.sleep(1000);
+            while(progressBar.getProgress() < 100) {
+                progressBar.incrementProgressBy(5);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }
