f6e9e7b712ac2b4972034e5efb99130a0d190f20
nam2626 <nam2626@gmail.com>
2023-07-21 10:13:22 +0900

    커스텀 리스트

--- 13_CustomListView/app/src/main/java/com/example/a13_customlistview/MainActivity.java
+++ 13_CustomListView/app/src/main/java/com/example/a13_customlistview/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a13_customlistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lst_view);
+        CustomListAdapter adapter = new CustomListAdapter();
+        adapter.addItem(new ListItem(getDrawable(R.drawable.facebook),"페이스북"));
+        adapter.addItem(new ListItem(getDrawable(R.drawable.instagram),"인스타그램"));
+
+        listView.setAdapter(adapter);
     }
 }
+
+
+
+
+
+
+
