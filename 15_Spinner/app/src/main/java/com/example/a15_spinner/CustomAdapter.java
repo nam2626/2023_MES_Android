b8519211cb450594c33bc5bb1c8baff8f4fd04a0
nam2626 <nam2626@gmail.com>
2023-07-21 12:51:26 +0900

    스피너

--- 15_Spinner/app/src/main/java/com/example/a15_spinner/CustomAdapter.java
+++ 15_Spinner/app/src/main/java/com/example/a15_spinner/CustomAdapter.java
@@ -1,28 +1,52 @@
 package com.example.a15_spinner;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CustomAdapter extends BaseAdapter {
+    private ArrayList<SpinnerItem> list;
+
+    public CustomAdapter() {
+        list = new ArrayList<SpinnerItem>();
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return list.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        final Context context = viewGroup.getContext();
+        if(view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.custom_spinner_item,viewGroup,false);
+        }
+
+        ImageView imageView = view.findViewById(R.id.img_national);
+        TextView textView = view.findViewById(R.id.txt_national);
+
+        SpinnerItem item = list.get(i);
+        imageView.setImageDrawable(item.getDrawable());
+        textView.setText(item.getText());
+
+        return view;
     }
 }
