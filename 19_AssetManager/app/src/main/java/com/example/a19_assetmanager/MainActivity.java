2ef55ed902d005ee68b4c8e9e61906e57f0cd3ca
nam2626 <nam2626@gmail.com>
2023-07-24 09:38:31 +0900

    에셋매니저

--- 19_AssetManager/app/src/main/java/com/example/a19_assetmanager/MainActivity.java
+++ 19_AssetManager/app/src/main/java/com/example/a19_assetmanager/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.a19_assetmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    final String TAG = "AssesManager";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView textView = findViewById(R.id.textView);
+
+        try {
+            AssetManager manager = getResources().getAssets();
+            Log.d(TAG, "onCreate: "+ Arrays.toString(manager.list("raw")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
