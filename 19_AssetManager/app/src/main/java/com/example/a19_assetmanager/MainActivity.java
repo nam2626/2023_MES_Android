0bbfc1a2f113964107a2e5a81a44506f3ac42201
nam2626 <nam2626@gmail.com>
2023-07-24 11:45:22 +0900

    카카오 검색

--- 19_AssetManager/app/src/main/java/com/example/a19_assetmanager/MainActivity.java
+++ 19_AssetManager/app/src/main/java/com/example/a19_assetmanager/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.a19_assetmanager;
 
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
         try {
             AssetManager manager = getResources().getAssets();
             Log.d(TAG, "onCreate: "+ Arrays.toString(manager.list("raw")));
+            InputStream is = manager.open("raw/desert.jpg");
+
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+//            imageView.setImageBitmap(bitmap);
+//            Glide.with(this).load(bitmap).into(imageView);
+            Glide.with(this).load("https://cdn.pixabay.com/photo/2023/06/23/19/34/campfire-8084064_1280.jpg").into(imageView);
+
+            //txt/한글.txt 내용을 전부 읽어와서 TextView에 출력
+            InputStream txtIn = manager.open("txt/한글.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(txtIn));
+            String msg = "";
+            String txt = "";
+            while((txt = br.readLine()) != null){
+                msg += txt + "\n";
+            }
+            textView.setText(msg);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
---
+++ 20_KaKaoSearch/app/src/main/java/com/codenation/MainActivity.java
@@ -0,0 +1,70 @@
+package com.codenation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class MainActivity extends AppCompatActivity {
+    final String TAG = "KaKaoSearch";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public class KaKaoSearch extends AsyncTask<Void, Void, String>{
+        private final String URL = "https://dapi.kakao.com/v2/search/image?query=";
+        private final String USER_INFO = "KakaoAK -";
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result = null;
+
+            try {
+                String search = URLEncoder.encode("갤럭시","UTF-8");
+                URL url = new URL(URL + search);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                //GET으로 전송하고 데이터를 받음
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Authorization",USER_INFO);
+                connection.setRequestProperty("content-type","application/json");
+                connection.setDoOutput(true);
+                connection.setUseCaches(false);
+                connection.setDefaultUseCaches(false);
+
+                //데이터를 받아오는 부분
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return result;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
